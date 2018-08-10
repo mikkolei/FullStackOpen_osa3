a58b8fc03e5667337e104c4fef7d0d7b449c9d19
Mikko Leinonen <mikko.leinonen91@gmail.com>
2018-08-10 15:57:15 +0300

    3.4 asti tehty, toiminnallisuus tarkistettu Postmanilla

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        name: 'Arto Hellas',
+        number: '040-123456',
+        id: 1
+    },
+    {
+        name: 'Martti Tienari',
+        number: '040-123456',
+        id: 2
+    },
+    {
+        name: 'Arto Järvinen',
+        number: '040-123456',
+        id: 3
+    },
+    {
+        name: 'Lea Kutvonen',
+        number: '040-123456',
+        id: 4
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const numberOfPersons = persons.length
+    const date = new Date()
+    res.send(`<p>puhelinluettelossa ${ numberOfPersons } henkilön tiedot</p> <p>${date}</p>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+  
+    if ( person ) {
+      res.json(person)
+    } else {
+      res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+  
+    res.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
